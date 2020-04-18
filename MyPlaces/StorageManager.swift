88071344406b9d7eb5a1bab1888ceadd6547eebc
Alexander Maklakov <mclakov@yandex.ru>
2020-04-18 17:59:04 +0300

    Работающее приложение, сохранение в БД realm, сортировка, строка поиска, создание, редактирование и удаление ресторанов

---
+++ MyPlaces/StorageManager.swift
@@ -0,0 +1,26 @@
+//
+//  StorageManager.swift
+//  MyPlaces
+//
+//  Created by Alexander on 18/04/2020.
+//  Copyright © 2020 Alexander. All rights reserved.
+//
+
+import RealmSwift
+
+let realm = try! Realm()
+
+class StorageManager {
+    
+    static func saveObject(_ place: Place) {
+        try! realm.write {
+            realm.add(place)
+        }
+    }
+    
+    static func deleteObject(_ place: Place) {
+        try! realm.write {
+            realm.delete(place)
+        }
+    }
+}
